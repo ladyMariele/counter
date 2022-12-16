71db0cc22b4c8a8212d820e304d29f1799368866
Mariele <lullalayyy@gmail.com>
2022-12-17 00:41:55 +0800

    add save button

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//initialize the count as 0
-//listen for when the increment button is clicked
-// increment the count variable when increment button is clicked
-// change the count-el id to reflect the current count variable
 
-count = 0
+let saveEl = document.getElementById("save-el")
+let count = 0
 
 function increment() {
     current = count++
-    document.getElementById("count-el").innerText = count
+    document.getElementById("count-el").textContent = count
 }
+
+
+function save() {
+    saveEl.textContent += count + " - "
+}
+
